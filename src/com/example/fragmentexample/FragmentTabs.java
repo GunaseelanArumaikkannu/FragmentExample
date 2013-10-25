6e9e523d1685652734e6dab5bb22c3026b58d596
Gunaseelan A <gunasiet@gmail.com>
2013-10-26 01:11:42 +0530

    Simple fragment example

---
+++ src/com/example/fragmentexample/FragmentTabs.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.fragmentexample;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+public class FragmentTabs extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		final ActionBar bar = getActionBar();
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+
+		bar.addTab(bar
+				.newTab()
+				.setText("Simple")
+				.setTabListener(
+						new TabListener<FragmentCalculate.SimpleAddition>(this,
+								"simple", FragmentCalculate.SimpleAddition.class)));
+		bar.addTab(bar
+				.newTab()
+				.setText("List")
+				.setTabListener(
+						new TabListener<SampleList.SampleListFragment>(
+								this, "list",
+								SampleList.SampleListFragment.class)));
+		if (savedInstanceState != null) {
+			bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
+	}
+
+	public static class TabListener<T extends Fragment> implements
+			ActionBar.TabListener {
+		private final Activity mActivity;
+		private final String mTag;
+		private final Class<T> mClass;
+		private final Bundle mArgs;
+		private Fragment mFragment;
+
+		public TabListener(Activity activity, String tag, Class<T> clz) {
+			this(activity, tag, clz, null);
+		}
+
+		public TabListener(Activity activity, String tag, Class<T> clz,
+				Bundle args) {
+			mActivity = activity;
+			mTag = tag;
+			mClass = clz;
+			mArgs = args;
+
+			mFragment = mActivity.getFragmentManager().findFragmentByTag(mTag);
+			if (mFragment != null && !mFragment.isDetached()) {
+				FragmentTransaction ft = mActivity.getFragmentManager()
+						.beginTransaction();
+				ft.detach(mFragment);
+				ft.commit();
+			}
+		}
+
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			if (mFragment == null) {
+				mFragment = Fragment.instantiate(mActivity, mClass.getName(),
+						mArgs);
+				ft.add(android.R.id.content, mFragment, mTag);
+			} else {
+				ft.attach(mFragment);
+			}
+		}
+
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			if (mFragment != null) {
+				ft.detach(mFragment);
+			}
+		}
+
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			new CustomToast(mActivity, "Reselected!");
+		}
+	}
+}
